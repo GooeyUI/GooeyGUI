1a281cc1f58af6785b0f1ceda8e7f1616ad867d4
YASSINE-AA <overflowtn@gmail.com>
2025-03-29 03:30:00 +0100

    GooeyBuilder: Implemented Images

--- examples/builder_example.c
+++ examples/builder_example.c
@@ -1,38 +1,31 @@
 #include <Gooey/gooey.h>
 
-// Callback functions
-void button_callback()
+void dropdown_callback(int selected_index)
 {
-    printf("Button clicked!\n");
-}
-
-void slider_callback(int value)
-{
-    printf("Slider value: %d\n", value);
-}
-
-void checkbox_callback(bool checked)
-{
-    printf("Checkbox %s\n", checked ? "checked" : "unchecked");
-}
-
-void input_callback(const char* text)
-{
-    printf("Input changed: %s\n", text);
+    printf("Dropdown selection changed to index: %d\n", selected_index);
 }
 
 int main()
 {
+
     Gooey_Init();
-    GooeyWindow *win = GooeyWindow_Create("Hello World", 800, 600, true);
+    GooeyWindow *win = GooeyWindow_Create("DropDown", 800, 800, true);
 
-    GooeyCanvas *canvas_0 = GooeyCanvas_Create(134, 90, 100, 30);
+    const char *fruits_options[] = {"apple", "banana", "manga", "melon"};
+    const char *quality_options[] = {"Low", "Medium", "High", "Ultra"};
+    const char *color_options[] = {"Red", "Green", "Blue", "Yellow", "Cyan", "Magenta"};
 
-    // Register all widgets with the window
-    GooeyWindow_RegisterWidget(win, canvas_0);
+    // Widgets for Options tab
+    GooeyDropdown *fruitsDropdown = GooeyDropdown_Create(130, 30, 150, 30,
+                                                         fruits_options, 4, dropdown_callback);
+    GooeyDropdown *qualityDropdown = GooeyDropdown_Create(30, 130, 150, 30,
+                                                          quality_options, 4, dropdown_callback);
+    GooeyDropdown *colorDropdown = GooeyDropdown_Create(30, 180, 150, 30, color_options, 6, dropdown_callback);
 
+    GooeyWindow_RegisterWidget(win, colorDropdown);
+    GooeyWindow_RegisterWidget(win, fruitsDropdown);
+  //  GooeyWindow_RegisterWidget(win, quality_options);
     GooeyWindow_Run(1, win);
     GooeyWindow_Cleanup(1, win);
-
     return 0;
 }
